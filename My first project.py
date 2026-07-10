b6a5e47cb67f410780ffa616e5d651b3adfbfc0e
Samson Lawrence <samsonlawrence58@gmail.com>
2026-07-10 01:48:25 +0100

    Add files via upload
    Added my first Python projects

---
+++ My first project.py
@@ -0,0 +1,19 @@
+name = "Sampson Lawrence Adanegbe"
+country = "Nigeria"
+state = "Rivers State"
+degree = "Bachelor of Science in Computer Science"
+current_skill = "Python Beginner"
+dream_career = "Cybersecurity Engineer"
+current_goal = "Earn my first online income"
+future_goal = "Work remotely and build multiple income streams"
+
+print("========== MY PROFILE ==========")
+
+print("Name:", name)
+print("Country:", country)
+print("State:", state)
+print("Degree:", degree)
+print("Current Skill:", current_skill)
+print("Dream Career:", dream_career)
+print("Current Goal:", current_goal)
+print("Future Goal:", future_goal)
---
+++ meet_user.py
@@ -0,0 +1,29 @@
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+
+state = input("What state do you live in? ")
+
+favourite_food = input("What is your favourite food? ")
+
+favourite_football_club = input("What is your favourite football club? ")
+
+dream_salary = input("What is your dream salary? ")
+
+print()
+
+print("========== WELCOME ==========")
+
+print("Hello,", name)
+
+print("Age:", age)
+
+print("State:", state)
+
+print("Favourite Food:", favourite_food)
+
+print("Favourite Football Club:", favourite_football_club)
+
+print("Dream Salary:", dream_salary)
+
+print(name + ", keep believing in yourself.")
